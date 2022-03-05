1a41ec188dd4ea477530ff6010652569c692c143
Yuriy Chebyshev <ychebyshev@it.ru>
2022-03-05 17:14:43 +0300

    parsing json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Read;
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("resources/mlp.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let parsed = json::parse(&*contents).unwrap();
+    println!("{}", parsed);
+    Ok(())
 }
