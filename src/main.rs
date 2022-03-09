0cd119ff677b91dbb1fa19b057f75f476edd5625
Yuriy Chebyshev <ychebyshev@it.ru>
2022-03-09 23:05:02 +0300

    smplificate error processing

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use crate::key::Key;
 
 fn main() -> std::io::Result<()> {
     let file = File::open("resources/mlp.json")?;
-    let parsed: Value = serde_json::from_reader(&file).expect("must be valid json");
+    let parsed: Value = serde_json::from_reader(&file)?;
     drop(file);
     let key = &parsed["levels"][0]["keys"][0];
-    let key1: Key = serde_json::from_value(key.clone()).expect("not contains key");
+    let key1: Key = serde_json::from_value(key.clone())?;
 
     println!("{}", key1);
     Ok(())
