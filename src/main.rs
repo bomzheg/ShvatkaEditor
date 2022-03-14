3c61c79f83f517fc4ccae5327b2c2a8bf23ff360
Yuriy Chebyshev <ychebyshev@it.ru>
2022-03-14 23:25:46 +0300

    normal modules structure

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+mod models;
+
 use std::fs::File;
 use serde_json::Value;
-use crate::key::Key;
+use crate::models::key::Key;
 
-#[path = "models/key.rs"] mod key;
 
 fn main() -> std::io::Result<()> {
-    let file = File::open("resources/mlp.json")?;
-    let parsed: Value = serde_json::from_reader(&file)?;
-    drop(file);
+    let parsed: Value = serde_json::from_reader(&File::open("resources/mlp.json")?)?;
     let key = &parsed["levels"][0]["keys"][0];
     let key1: Key = serde_json::from_value(key.clone())?;
 
